fd5df6193a5cbcc157208eb45c09becc5a057905
Ashim Nepal <143585311+ashim-nepal@users.noreply.github.com>
2023-10-04 21:49:24 +0545

    Add files via upload

---
+++ euclideanGCD_05/GCD_algo.java
@@ -0,0 +1,33 @@
+package euclideanGCD_05;
+import java.lang.*;
+import java.util.*;
+/*
+ * Euclidean Algorithm
+ * By Ashim Nepal
+ */
+
+public class GCD_algo {
+	// Additional Euclidean Algorithm
+	public static int gcd(int a, int b) {
+		if(a==0) {
+			return b;
+		}
+		return gcd(b%a,a);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the Number of times to gind GCD of two numbers: ");
+		int times=sc.nextInt();	
+		for (int i=0; i<times; i++) {
+			System.out.print("Enter number 'a': ");
+			int a = sc.nextInt();
+			System.out.print("Enter number 'b': ");
+			int b = sc.nextInt();
+		
+			int g = gcd(a,b);
+			System.out.println("GCD(" +a+" , "+b+") = "+g);
+			}
+		}
+
+}
