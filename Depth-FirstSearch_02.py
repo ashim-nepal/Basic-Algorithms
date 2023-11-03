3dafc02ca5ef21f83965ceb07915b2e603c5c06c
Ashim Nepal <143585311+ashim-nepal@users.noreply.github.com>
2023-11-03 20:42:02 +0545

    Update Depth-FirstSearch_02.py

--- Depth-FirstSearch_02.py
+++ Depth-FirstSearch_02.py
@@ -6,8 +6,9 @@ class Graph:
     # Constructor
     def __init__(self):
         self.graph=defaultdict(list)
-
+    #
     # Function to add an edge to graph
+    #
 
     def addEdge(self,u,v):
         self.graph[u].append(v)
