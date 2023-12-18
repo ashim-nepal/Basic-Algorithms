fa59466712d4893d4cb117518aca2c27fcdef5f6
Ashim Nepal <143585311+ashim-nepal@users.noreply.github.com>
2023-12-18 20:23:33 +0545

    Update eulersTotient.py

--- Eulers_Totient_06/eulersTotient.py
+++ Eulers_Totient_06/eulersTotient.py
@@ -4,10 +4,8 @@
 - Euler's Totient Function
 - Ashim Nepal
 '''
-#
 # We will be using Euclidean GCD algorithm too
 # I had done it Euclidean GCD previously in java and it is what it looks in python
-#
 def gcd(a,b):
     if (a==0):
         return b
